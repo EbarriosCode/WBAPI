6713afa3f511e31012df7e21faa18f7486f5f7c7
Eduardo Barrios <guayoswing@gmail.com>
2026-05-27 03:36:21 -0600

    Agregando politicas de cors para acceso desde el dominio de la aplicación web blazor wasm

--- WBAPI.Insfrastructure/DependencyContainer.cs
+++ WBAPI.Insfrastructure/DependencyContainer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using WBAPI.Application.Interfaces;
 using WBAPI.Domain.Interfaces;
 using WBAPI.Infrastructure.Implementations.Common;
@@ -20,7 +21,10 @@ namespace WBAPI.Insfrastructure
             services.AddDbContext<AppDbContext>(opts =>
                 opts.UseSqlServer(
                     configuration.GetConnectionString("DefaultConnection"),
-                    b => b.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName)));
+                    b => b.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName))
+                    .EnableDetailedErrors()
+                    .EnableSensitiveDataLogging()
+                    .LogTo(Console.WriteLine, LogLevel.Information));
            
             services
                 .AddIdentity<AppUser, IdentityRole>(opts =>
